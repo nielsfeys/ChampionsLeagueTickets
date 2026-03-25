36db897805b3fd0c0c9942856a2fe389ccfff5a7
sherbert1994 <feysniels@gmail.com>
2026-03-25 14:46:06 +0100

    blargh

---
+++ ChampionsLeagueTickets.Domain/Entities/AspNetRole.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ChampionsLeagueTickets.Domain.Entities;
+
+public partial class AspNetRole
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public string ConcurrencyStamp { get; set; }
+
+    public virtual ICollection<AspNetRoleClaim> AspNetRoleClaims { get; set; } = new List<AspNetRoleClaim>();
+
+    public virtual ICollection<AspNetUser> Users { get; set; } = new List<AspNetUser>();
+}
